507528505520601f94e1baaa6570817195ec400b
ChloeShanshanXu <chloexu@umich.edu>
2022-01-30 11:48:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def _binary_search(ls, num_to_find, start_index, end_index):
 
     mid=(start_index + end_index)//2
     if ls[mid]==num_to_find:
-        return (mid)
+        return mid
     if ls[mid]<num_to_find:
         return _binary_search(ls, num_to_find, mid+1, end_index)
     return _binary_search(ls, num_to_find, start_index, mid-1)
@@ -24,10 +24,36 @@ def _binary_search(ls, num_to_find, start_index, end_index):
 
 def binary_search(ls, num_to_find):
     return _binary_search(ls, num_to_find,0,len(ls)-1)
-    print("number is at " + mid + "place of the list")
 
 if __name__ =='__main__':
     numbers=[2,54,23,93,7,43, 90, 203,4,8]
     numbers.sort()
     print(numbers)
     print("number is at " + str(binary_search(numbers, 54)) + " place of the list")
+
+def iterative_binary_search(ls,num_to_find):
+    start_index = 0
+    end_index=len(ls)-1
+    while start_index <= end_index:
+        mid = (start_index + end_index)//2
+        if ls[mid]== num_to_find:
+            return mid
+            quit
+        if ls[mid]> num_to_find:
+            end_index=mid-1
+        else:
+            start_index = mid+1
+
+
+
+
+
+
+if __name__ =='__main__':
+    numbers=[2,54,23,93,7,43, 90, 203,4,8]
+    numbers.sort()
+    print(iterative_binary_search(numbers, 54))
+    print("number is at " + str(iterative_binary_search(numbers, 54)) + " place of the list")
+
+    for value in numbers:
+        print("the index of {} in the list is:{}".format(value, binary_search(numbers, value)))
