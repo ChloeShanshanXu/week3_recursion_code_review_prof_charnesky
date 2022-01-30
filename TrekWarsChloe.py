dfb2eb26c03a1a38017325b4f859063e2975cff0
ChloeShanshanXu <chloexu@umich.edu>
2022-01-30 18:53:41 -0500

    Update TrekWarsChloe.py

--- TrekWarsChloe.py
+++ TrekWarsChloe.py
@@ -1,11 +1,12 @@
 from enum import Enum
-
+#code reference from: https://docs.python.org/3.11/howto/enum.html
 class Alignment(Enum):
     us = 1
     them = 2
     chaotic = 3
 
 class Ship:
+
     def __init__(self, name, x, y, alignment,max_health, range, attack_power):
         self._name = name
         self._x_location = x
@@ -15,3 +16,16 @@ class Ship:
         self._range = range
         self._attack_power = attack_power
         self._current_health = self._max_health
+
+    def _attack(self, target):
+        if target _is_in_range:
+            target._current_health -= self._attack_power
+        else:
+            raise ValueError("Ship out of range")
+
+    def _is_in_range:
+        dist = sqrt((self._x_location - target._x_location) ^ 2 + (self._y_location - target._y_location) ^ 2)
+        if dist > range:
+            return False
+        else:
+            return True
