2e7927e59df281137b97fdcfdc22a5cc71d71bba
ChloeShanshanXu <chloexu@umich.edu>
2022-01-30 10:52:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,22 @@ if __name__ =='__main__':
     count_down_loop(8)
 
 def _binary_search(ls, num_to_find, start_index, end_index):
-    if start_index>end_index:
-        raise ValueError(str(num_to_find) + " is not in the list")
+
     mid=(start_index + end_index)//2
     if ls[mid]==num_to_find:
-        print(mid)
+        return (mid)
     if ls[mid]<num_to_find:
         return _binary_search(ls, num_to_find, mid+1, end_index)
     return _binary_search(ls, num_to_find, start_index, mid-1)
 
+
+
 def binary_search(ls, num_to_find):
     return _binary_search(ls, num_to_find,0,len(ls)-1)
+    print("number is at " + mid + "place of the list")
 
 if __name__ =='__main__':
-    numbers=[2,54,23,93,2,43, 90, 203,4,8]
+    numbers=[2,54,23,93,7,43, 90, 203,4,8]
     numbers.sort()
-    binary_search(numbers, 54)
+    print(numbers)
+    print("number is at " + str(binary_search(numbers, 54)) + " place of the list")
