f5c48caae40aa6afb07b05fb03bcaa73a64f15d4
ChloeShanshanXu <chloexu@umich.edu>
2022-01-30 18:32:23 -0500

    Update TrekWarsChloe.py

--- TrekWarsChloe.py
+++ TrekWarsChloe.py
@@ -1,5 +1,17 @@
 from enum import Enum
+
 class Alignment(Enum):
     us = 1
     them = 2
     chaotic = 3
+
+class Ship:
+    def __init__(self, name, x, y, alignment,max_health, range, attack_power):
+        self._name = name
+        self._x_location = x
+        self._y_location = y
+        self._alignment = alignment
+        self._max_health = max_health
+        self._range = range
+        self._attack_power = attack_power
+        self._current_health = self._max_health
