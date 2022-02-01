53726323d3fde0cebbf3e045c5d04a89e12408b9
ChloeShanshanXu <chloexu@umich.edu>
2022-02-01 10:07:36 -0500

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -1,3 +1,18 @@
 class NQueens:
 
     def __init__(self, number_of_queens=8):
+        self._total_queens = number_of_queens
+        self._current_number_of_queens=0
+        self._board = []
+        self._number_of_step = 0
+        self._number_of_solutions = 0
+        for row in range (number_of_queens):
+            self._board.append([])
+            for column in range(number_of_queens):
+                self._board[row].append(' ')
+        self.solve()
+
+    def __str__(self):
+        return "\n".join(str(row) for row in self._board)+"\n"\
+               + "Solution number:"+ str(self._number_of_solutions)+"\n"\
+               + "Number of steps:" + str(self._number_of_step)
