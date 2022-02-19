da0eddd5bce7c05f190422e9a51d2fed0da23c73
ChloeShanshanXu <chloexu@umich.edu>
2022-02-18 23:53:39 -0500

    update

--- MazeSolver.py
+++ MazeSolver.py
@@ -11,3 +11,17 @@ class MazeSolver:
             for col_index in range(len(self._maze[row_index])):
                 if self._maze[row_index][col_index] == "S":
                     self._solve(row_index, col_index)
+
+    def shortest_path(self):
+        try:
+            return self._steps_per_solution[min(self._steps_per_solution.keys())]
+        except ValueError as e:
+            return "No Solution Found"
+
+    def solve(self, row_index, col_index):
+        if self._maze[row_index][col_index] == "E"
+            self._steps_per_solution[self._current_steps] = str(self)
+            return
+        if self._maze[row_index][col_index] != 's':
+            self._maze[row_index][col_index] = "X"
+        self._current_steps += 1
